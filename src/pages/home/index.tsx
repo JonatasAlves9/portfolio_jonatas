77cb6dfd8d06507e484f3bd5e4a0af456263bc58
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-06-29 17:32:15 -0300

    Identaçao de codigo e inicio de construçao da pagina

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <div className="box_left">
+          <h1 className="titleOla" >Olá</h1>
+          <h3 className="subTitleName" >Meu nome é</h3>
+          <h1 className="titleName" >Jônatas Alves</h1>
+          <button className="buttonSkills" >Skills</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
