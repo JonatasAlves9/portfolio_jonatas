259cb498f94b975eee3bb679d7f83ae59ef8c15d
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-06-29 21:47:03 -0300

    Componetizaçao das paginas para executar as logicas

---
+++ src/pages/components/box_left.tsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+
+export default class boxLeft extends Component<any, any> {
+    render() {
+        return (<div className="App">
+        <div className="box_left">
+            <h1 className="titleOla" >Olá</h1>
+            <h3 className="subTitleName" >Meu nome é</h3>
+            <h1 className="titleName" >Jônatas Alves</h1>
+            <button className="buttonSkills" >Skills</button>
+        </div>
+  
+        <div className="box_right">
+            <div className="image"></div>
+            <h3 className="name">Jônatas Alves</h3>
+            <h3 className="description">Desenvolvedor Web</h3>
+  
+            <div className="info">
+              <p className="text">Email: alvesjonatas99@gmail.com</p>
+              <p className="text">Telefone: (81)99252-8586</p>
+              <p className="text">Caruaru - Pernambuco - Brasil</p>
+            </div>
+  
+            <div className="links">
+              <p className="text">Github: www.example.com</p>
+              <p className="text">Linkedin: www.example.com</p>
+              <p className="text">Currículo: www.example.com</p>
+            </div>
+        </div>
+  
+   
+  
+      </div>
+      )
+    }
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import './styles.css';
+import BoxLeft from '../components/box_left';
+import Page2 from '../components/page2';
+
+const isClicked = true;
 
 function Home() {
   return (
-    <div className="App">
-      <div className="box_left">
-          <h1 className="titleOla" >Olá</h1>
-          <h3 className="subTitleName" >Meu nome é</h3>
-          <h1 className="titleName" >Jônatas Alves</h1>
-          <button className="buttonSkills" >Skills</button>
-      </div>
-      <div className="box_right">
-          
-      </div>
-    </div>
+
+    isClicked ? 
+      <>
+      <BoxLeft></BoxLeft>
+      </>
+      : <Page2></Page2>
   );
 }
 
